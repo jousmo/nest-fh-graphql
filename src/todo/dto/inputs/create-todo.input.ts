dc8b04523c726645d0822a514f7f24ad5b9e331f
José Uscanga <jousmo@outlook.com>
2023-07-05 20:45:48 -0600

    feat: implement validation with class validation and global pipe validation

--- src/todo/dto/inputs/create-todo.input.ts
+++ src/todo/dto/inputs/create-todo.input.ts
@@ -1,10 +1,15 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsBoolean, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 @InputType()
 export class CreateTodoInput {
   @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(20)
   description: string;
 
   @Field(() => Boolean)
+  @IsBoolean()
   done: boolean;
 }
