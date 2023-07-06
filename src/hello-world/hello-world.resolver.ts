2022ebaa708cba81911d10e6cb09e9158175b93f
José Uscanga <jousmo@outlook.com>
2023-07-05 18:52:04 -0600

    feat: example create new query using description and naming

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,17 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
   @Query(() => String)
   helloWorld(): string {
     return 'Hola mundo';
+  }
+
+  @Query(() => Float, {
+    description: 'Dara un numero aleatorio',
+    name: 'randomNumber',
+  })
+  calculateRandomNumber(): number {
+    return Math.random() * 100;
   }
 }
